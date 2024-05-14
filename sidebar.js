c3256952b71ecf35d1260cace046f699b09033ea
Nguyen Tuan Huy <tuanhuy03ns@gmail.com>
2024-05-14 22:02:26 +0700

    init

---
+++ sidebar.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var menuItems = document.querySelectorAll('.sidebar-menu > li > a');
+    menuItems.forEach(function (menuItem) {
+        menuItem.addEventListener('click', function (e) {
+            var icons = menuItem.querySelector('.chevron-icon');
+            var submenu = menuItem.nextElementSibling;
+
+            if (submenu && submenu.classList.contains('submenu')) {
+                e.preventDefault();
+                submenu.style.display = submenu.style.display === 'block' ? 'none' : 'block';
+            }
+            if (icons) {
+                icons.style.transform = icons.style.transform === 'rotate(90deg)' ? 'rotate(0deg)' : 'rotate(90deg)';
+            }
+        });
+    });
+});
