ab34e5301d8bca3b537d3dc2a6d3845dcaf92217
gnud0505 <vandung0505nb@gmail.com>
2024-05-29 12:57:45 +0700

    update howtouse

--- helpkpilist.js
+++ helpkpilist.js
@@ -9,3 +9,14 @@ overlay.addEventListener("click", function(){
     helpKpiList.style.display="none";
     helpKpiCreate.style.display="block";
 });
+
+window.onclick = function(event) {
+    if (!event.target.matches('.help-content .close') && !event.target.matches('.dropdown *')) {
+        const dropdownMenu = document.querySelector('.dropdown-menu');
+        dropdownMenu.style.display = 'none';
+    }
+}
+
+function closeHelp(){
+    window.location.href = "howtouse.html";
+}
